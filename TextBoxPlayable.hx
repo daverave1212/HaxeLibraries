52658853d8bdb555511e50c9e42c4c9f965f9e53
daverave1212 <36113284+daverave1212@users.noreply.github.com>
2019-01-09 07:29:58 -0800

    Updated to work with the updated TextBox

--- TextBoxPlayable.hx
+++ TextBoxPlayable.hx
@@ -1,14 +1,86 @@
-//package scripts;
-
 /*
+    Stencyl exclusively uses the Haxe programming language. 
+    Haxe is similar to ActionScript and JavaScript.
+    
+    Want to use native code or make something reusable? Use the Extensions Framework instead.
+    http://www.stencyl.com/help/view/engine-extensions/
+    
+    Learn more about Haxe and our APIs
+    http://www.stencyl.com/help/view/haxe/
+*/
+
+package scripts;
+
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
 import com.stencyl.behavior.Script;
 import com.stencyl.behavior.Script.*;
-import com.stencyl.Engine;
-import com.stencyl.models.Font;
-import com.stencyl.graphics.G;
+import com.stencyl.behavior.ActorScript;
 import com.stencyl.behavior.SceneScript;
 import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+import com.stencyl.models.Joystick;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
 import com.stencyl.utils.Utils;
+
+import openfl.ui.Mouse;
+import openfl.display.Graphics;
+import openfl.display.BlendMode;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.events.TouchEvent;
+import openfl.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+/*
 		Extra API
 	.play()				starts playing the text from the beginning
 	.stop()				stops playing the text
@@ -79,8 +151,23 @@ class TextBoxPlayable extends TextBox{
 
 	private override function draw(g : G){
 		if(isDrawing){
+			if(drawOutline){
+				g.strokeColor = Utils.getColorRGB(255,200,0);
+				g.strokeSize = 4;
+				g.drawRect(x, y, w, h);
+			}
 			g.setFont(font);
 			for(currentLine in 0...currentLine + 1){
-				g.drawString(playingText[currentLine], x, y + lineSpacing * currentLine);}}}
+				var drawx = x;
+				var drawy = y;
+				if(centerHorizontally){
+					drawx = x + (w - font.font.getTextWidth(lines[currentLine]))/2;
+				}
+				if(centerVertically){
+					drawy = y + (h - lineSpacing * nLines) / 2 + lineSpacing * currentLine;
+				} else {
+					drawy = y + lineSpacing * currentLine;
+				}
+				g.drawString(playingText[currentLine], drawx, drawy);}}}
 
 }
