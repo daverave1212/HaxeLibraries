7f8e354f957a98bc0619f5ff9710aae5ad0b77e1
daverave1212 <36113284+daverave1212@users.noreply.github.com>
2019-01-09 07:13:33 -0800

    Can now center vertically and horizontally
    Also,  uncommented the package and imports at the top

--- TextBox.hx
+++ TextBox.hx
@@ -1,11 +1,75 @@
-//package scripts;
+package scripts;
 
-/*
-import com.stencyl.Engine;
-import com.stencyl.models.Font;
+
+
 import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.Script.*;
+import com.stencyl.behavior.ActorScript;
 import com.stencyl.behavior.SceneScript;
-*/
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+import com.stencyl.models.Joystick;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.Utils;
+
+import openfl.ui.Mouse;
+import openfl.display.Graphics;
+import openfl.display.BlendMode;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.events.TouchEvent;
+import openfl.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
 
 /*
  * TextBox:
@@ -13,6 +77,8 @@ import com.stencyl.behavior.SceneScript;
  * Use .setText(String) to set it's text
  * Then simply call startDrawing()
  * Use stopDrawing() to stop drawing
+ *
+ * You can also set .centerVertically .centerHorizontally and .drawOutline to true!
  *
  * NOTE: Don't forget to uncomment the package at the top of the code!
  */
@@ -40,15 +106,19 @@ import com.stencyl.behavior.SceneScript;
 
 class TextBox extends SceneScript{
 	
-	var lines 		:Array<Dynamic>;
-	var w			:Int = 400;
-	var h			:Int = 400;
-	var x			:Float = 50;
-	var y			:Float = 50;
-	var nLines		:Int = 0;
-	var lineSpacing	:Float = 20;
-	var font		:Font;
-	var isDrawing	:Bool = false;
+	public var lines 		:Array<Dynamic>;
+	public var w			:Int = 400;
+	public var h			:Int = 400;
+	public var x			:Float = 50;
+	public var y			:Float = 50;
+	public var nLines		:Int = 0;
+	public var lineSpacing	:Float = 20;
+	public var font			:Font;
+	public var isDrawing	:Bool = false;
+	
+	public var centerHorizontally	:Bool = false;
+	public var centerVertically 	:Bool = false;
+	public var drawOutline 			:Bool = false;
 	
 	private var text		:String;
 	
@@ -104,9 +174,26 @@ class TextBox extends SceneScript{
 		
 	private function draw(g : G){
 		if(isDrawing){
+			if(drawOutline){
+				g.strokeColor = Utils.getColorRGB(255,200,0);
+				g.strokeSize = 4;
+				g.drawRect(x, y, w, h);
+			}
 			g.setFont(font);
 			for(currentLine in 0...nLines){
-				g.drawString(lines[currentLine], x, y + lineSpacing * currentLine);}}}
+				var drawx = x;
+				var drawy = y;
+				if(centerHorizontally){
+					drawx = x + (w - font.font.getTextWidth(lines[currentLine]))/2;
+				}
+				if(centerVertically){
+					drawy = y + (h - lineSpacing * nLines) / 2 + lineSpacing * currentLine;
+				} else {
+					drawy = y + lineSpacing * currentLine;
+				}
+				g.drawString(lines[currentLine], drawx, drawy);
+				
+			}}}
 	
 	public function startDrawing(){
 		isDrawing = true;}
